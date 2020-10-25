da2faa62068d0729c619eab8125e5ffe51f0ef3f
Marianne Le <73395856+iridazzle-berry@users.noreply.github.com>
2020-10-25 09:46:17 -0500

    Update covidapi.js

--- covidapi.js
+++ covidapi.js
@@ -1,14 +1,21 @@
-$.getJSON("https://api.covidtracking.com/v1/us/daily.json",function getPositive(data){
-    let positive = data[0].positive;
-    $(".positive").append(positive);
-    let negative = data[0].negative;
-    $(".negative").append(negative);
-    let death = data[0].death;
-    $(".death").append(death);
-    let deathIncrease = data[0].deathIncrease;
-    $(".deathIncrease").append(deathIncrease);
-    let hospitalized = data[0].hospitalized;
-    $(".hospitalized").append(hospitalized);
-    let recovered = data[0].recovered;
-    $(".recovered").append(recovered);
-});
+function getState(){
+    var stateCode = document.getElementById("stateCode").defaultValue = "us"
+    return "https://api.covidtracking.com/v1/" + stateCode + "/daily.json"
+};
+
+function getStats(){
+    $.getJSON(getState(),function(data){
+        let positive = data[0].positive;
+        $(".positive").append(positive);
+        let negative = data[0].negative;
+        $(".negative").append(negative);
+        let death = data[0].death;
+        $(".death").append(death);
+        let deathIncrease = data[0].deathIncrease;
+        $(".deathIncrease").append(deathIncrease);
+        let hospitalized = data[0].hospitalized;
+        $(".hospitalized").append(hospitalized);
+        let recovered = data[0].recovered;
+        $(".recovered").append(recovered);
+    });
+};
